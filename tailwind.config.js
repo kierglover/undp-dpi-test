4f20c049163cb9feb1100456c1bf291dc802b66e
mad-hengseyha <108649284+mad-hengseyha@users.noreply.github.com>
2023-12-14 14:57:51 +0700

    update feedback from client

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         // 'sans': ['Proxima-Nova'],
         // 'bold': ['Proxima-Nova-Bold'],
       },
+      colors:{
+        'un': {'un-blue':'#006EB5', 'un-white':'#FFFFFF', 'gray-700': '#232E3D'},
+        'header':{
+          "bg-color":"#fafafa",
+          "box-shadow":" 0 3px 6px rgba(0,0,0,.1)"
+        }
+      },
+      opacity:{
+        '99': '.99',
+      }
     },
   },
   plugins: [],
